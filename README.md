dda4c59061c17320337dc8d797fcf3fb9f0fefa6
Cal <callumburnsoregan@gmail.com>
2025-01-15 01:59:55 +0000

    15/1/25

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # iot-backend
 
 This is the arduino side for the IoT project.
+Please change these fields to reflect your ssid, password, ip and port.
+
+```c
+// WiFi credentials
+const char* ssid = "staline"; // Replace with your network name
+const char* password = "mdptemporaire1"; // Replace with your network password
+
+// Server details
+const char* serverIP = "172.20.10.5";  // Replace with your flask server ip
+const int serverPort = 5000;           // Replace with your flask server port
+```
--- WifiServer/WifiServer.ino
+++ WifiServer/WifiServer.ino
@@ -1,12 +1,12 @@
 #include <WiFiS3.h>
 
 // WiFi credentials
-const char* ssid = "staline";
-const char* password = "mdptemporaire1";
+const char* ssid = "staline"; // Replace with your network name
+const char* password = "mdptemporaire1"; // Replace with your network password
 
 // Server details
-const char* serverIP = "172.20.10.5";  // Replace with your ip
-const int serverPort = 5000;           // Replace with your port
+const char* serverIP = "172.20.10.5";  // Replace with your flask server ip
+const int serverPort = 5000;           // Replace with your flask server port
 
 const int pin = 7;
 int state = 0;
